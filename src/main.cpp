cec1b93591f933e754185e2d378ac40789d2eac6
zainalwan <zainalwan4@gmail.com>
2021-07-05 23:10:32 +0700

    Change main.cpp
    - Change main() argument
    - Create say_hello()
    - Create print_argument_information(...)
    - Use the new two function inside main()

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 
-int main()
+void say_hello();
+void print_argument_information(int &arguments_count, const char* const
+    arguments_value[]);
+
+int main(int arguments_count, const char* const arguments_value[])
 {
-    std::cout << "Hello, World" << '\n';
+    say_hello();
+    print_argument_information(arguments_count, arguments_value);
 
     return 0;
+}
+
+void say_hello()
+{
+    std::cout << "Hello, World" << '\n';
+}
+
+void print_argument_information(int &arguments_count, const char* const
+    arguments_value[])
+{
+    std::cout << "The total passed arguments: " << arguments_count << '\n';
+    std::cout << "Arguments list:" << '\n';
+    for(short i = 0; i < arguments_count; i++)
+    {
+        std::cout << "    - " << arguments_value[i] << '\n';
+    }
 }
